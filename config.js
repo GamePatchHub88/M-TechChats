973c69ca34fa5aa3fdfefbee4adbe61e7e884e0b
Mohamed Ahmed Helmy Aboura <mh3976698@gmail.com>
2026-07-05 19:24:18 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// ⚠️ ملف الإعدادات الحساسة — لا ترفعه على GitHub أو أي مكان عام
+// أضف هذا الملف إلى .gitignore
+
+const APP_CONFIG = {
+  supabase: {
+    url: "https://efojlqpwiyqnpdxvkyiq.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmb2pscXB3aXlxbnBkeHZreWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NDE3NDQsImV4cCI6MjA4OTUxNzc0NH0.K6-_JxruqbUEb731e_uu9LzGQDuJ5dUAkAvcK1V-IdA",
+  },
+};
